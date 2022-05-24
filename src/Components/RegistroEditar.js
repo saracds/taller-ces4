3e40e3913a7505ebf4ba0b06316202b61f32cac1
saracds <saracardenas0618@gmail.com>
2022-05-23 19:42:00 -0500

    validaciones

--- src/Components/RegistroEditar.js
+++ src/Components/RegistroEditar.js
@@ -1,10 +1,25 @@
+import React, { useState } from 'react';
+
 import { Form, Button, Row, Col, Card, InputGroup, FormControl } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import React from 'react';
+
 
 const RegistroEditar = ({ movimiento, handleMovimiento }) => {
 
   const { tipo_movimiento, nombre, cantidad } = movimiento;
+
+  const [validated, setValidated] = useState(false);
+
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+
+    setValidated(true);
+  };
+
 
   return (
     <Card className='text-center'>
@@ -12,14 +27,18 @@ const RegistroEditar = ({ movimiento, handleMovimiento }) => {
         <Card.Title>Registro</Card.Title>
         <hr />
         <Card.Text>
-          <Form className='text-center'>
-            <Form.Group className='col'>
+          <Form className='text-center' noValidate validated={validated} onSubmit={handleSubmit}>
+            <Form.Group className='col' controlId="validationTipoMovimiento">
               <Row>
                 <Col md="4">
                   <Form.Label>Tipo Movimiento</Form.Label>
                 </Col>
                 <Col md='8'>
-                  <Form.Select name = "tipo_movimiento" onChange={e => handleMovimiento("tipo_movimiento", e.target.value)} >
+                  <Form.Select
+                    name="tipo_movimiento"
+                    onChange={e => handleMovimiento("tipo_movimiento", e.target.value)}
+                    required
+                  >
                     <option>Seleccione una opción</option>
                     <option value="Ingreso">Ingreso</option>
                     <option value="Gasto">Gasto</option>
@@ -28,28 +47,46 @@ const RegistroEditar = ({ movimiento, handleMovimiento }) => {
               </Row>
             </Form.Group>
             <br />
-            <Form.Group className='col'>
+            <Form.Group className='col' controlId="validationNombre">
               <Row>
                 <Col md="4">
                   <Form.Label>Nombre</Form.Label>
                 </Col>
                 <Col md='8'>
                   <InputGroup className="mb-3">
-                    <FormControl type='text' placeholder='Nombre' name="nombre" value={nombre} onChange={e => handleMovimiento("nombre", e.target.value)} />
+                    <FormControl
+                      type='text'
+                      placeholder='Nombre'
+                      name="nombre"
+                      value={nombre}
+                      onChange={e => handleMovimiento("nombre", e.target.value)}
+                      required
+                    />
                   </InputGroup>
                 </Col>
               </Row>
+              <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
             </Form.Group>
             <br />
-            <Form.Group className='col'>
+            <Form.Group className='col' controlId="validationCantidad">
               <Row>
                 <Col md='4'>
                   <Form.Label>Cantidad</Form.Label>
                 </Col>
                 <Col md='8'>
-                  <Form.Control type='number' placeholder='Costo' name="cantidad" value={cantidad} onChange={e => handleMovimiento("cantidad", e.target.value)} />
+                  <Form.Control
+                    type='number'
+                    placeholder='Costo'
+                    name="cantidad"
+                    value={cantidad}
+                    onChange={e => handleMovimiento("cantidad", e.target.value)}
+                    required
+                  />
                 </Col>
               </Row>
+              <Form.Control.Feedback type="invalid">
+              Please choose a .
+            </Form.Control.Feedback>
             </Form.Group>
 
             <br />
